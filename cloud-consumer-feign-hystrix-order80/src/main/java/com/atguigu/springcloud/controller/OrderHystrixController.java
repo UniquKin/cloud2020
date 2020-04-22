e02994b7136225150de2e13d795418b7bc4200fc
zhangruchao.pn@unicloud.com <zhangruchao.pn@unicloud.com>
2020-04-22 18:09:46 +0800

    看完59集内容

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,9 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystrixController {
 
     @Resource
@@ -22,8 +26,22 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+//    @HystrixCommand(fallbackMethod="paymentInfo_TimeOutFallbackMethod",commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+//    })
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        int age = 10/0;
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
+    }
+
+    public String paymentInfo_TimeOutFallbackMethod(@PathVariable("id") Integer id){
+        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己，/(ㄒoㄒ)/~~";
+    }
+
+    //下面是全局的fallback方法
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
     }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,13 +11,20 @@ public class PaymentService {
         return "线程池：\t"+Thread.currentThread().getName()+"\tpaymentInfo_OK,id:\t"+id+"\t"+"O(∩_∩)O哈哈~";
     }
 
+    @HystrixCommand(fallbackMethod="paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber = 3;
+//        int age = 10/0;
         try {
-            Thread.sleep(timeNumber*1000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "线程池：\t"+Thread.currentThread().getName()+"\tpaymentInfo_TimeOut,id:\t"+id+"\t"+"O(∩_∩)O哈哈~"+"耗时："+timeNumber;
+        return "线程池：\t"+Thread.currentThread().getName()+"\tid:\t"+id+"\t"+"O(∩_∩)O哈哈~"+"耗时：";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池：\t"+Thread.currentThread().getName()+"\t8001系统繁忙或者运行报错，请稍后再试,id:\t"+id+"\t"+"/(ㄒoㄒ)/~~";
     }
 }

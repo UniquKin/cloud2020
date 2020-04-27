8e1657d17bc0ac8f42d66cc6178e34a15714db3a
zhangruchao.pn@unicloud.com <zhangruchao.pn@unicloud.com>
2020-04-27 18:05:34 +0800

    看完81集内容

--- cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * curl -X POST "http://localhost:3344/actuator/bus-refresh"
+ * 第二种设计方式刷新3344服务端利用消息插件全部通知
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigCenterMain3344 {
---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * curl -X POST "http://localhost:3355/actuator/refresh"
+ * 动态更新用cmd发送post请求（手动版）
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}

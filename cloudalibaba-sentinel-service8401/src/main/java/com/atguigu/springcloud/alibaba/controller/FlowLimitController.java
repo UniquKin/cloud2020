a5caaf3503cede50bf7b6bc314a6aac90cc49127
EverythingGoesWithTheFlow <296536358@qq.com>
2020-05-24 12:37:07 +0800

    看完122集内容

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -11,13 +11,33 @@ public class FlowLimitController {
     @GetMapping("/testA")
     public String testA()
     {
+        log.info(Thread.currentThread().getName()+"\t"+"...testA");
         return "------testA";
     }
 
     @GetMapping("/testB")
     public String testB()
     {
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
         return "------testB";
+    }
+
+    /**
+     * RT设定值是处理时间
+     * RT默认是1秒5个线程处理时间是设定的RT值。这两个全部未达标会访问失败
+     * JMeter设置1秒10个线程，RT设置的值是200毫秒所以会报错
+     * @return
+     */
+    @GetMapping("/testD")
+    public String testD(){
+        //暂停几秒钟线程
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "------testD";
     }
 
 }

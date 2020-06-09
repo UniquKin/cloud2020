2ef9a1b5370126307cd8e148ade2c7f208df46b7
EverythingGoesWithTheFlow <296536358@qq.com>
2020-06-10 01:28:28 +0800

    看完124集内容

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,11 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,13 +35,34 @@ public class FlowLimitController {
     @GetMapping("/testD")
     public String testD(){
         //暂停几秒钟线程
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        log.info("testD 测试RT");
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info("testD 测试RT");
+
+        log.info("testD 异常比例");
+        int age = 10/0;
         return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE(){
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "------testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,/(ㄒoㄒ)/~~";   //sentinel系统默认的提示：Blocked by sentinel (flow limiting)
     }
 
 }
